a34bf1d9a538469753a32af5e7cd23a116349495
Andy <aledesmacc@gmail.com>
2022-07-05 22:46:59 -0400

    build: worker docker file and system entry point
    Docker file for worker role. Docker-compose file.

---
+++ tournament/worker.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/CSProjectsAvatar/distri-systems/tournament/domain"
+	"github.com/CSProjectsAvatar/distri-systems/tournament/domain/chord"
+	"github.com/CSProjectsAvatar/distri-systems/tournament/infrastruct"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	var entry *chord.RemoteNode
+	log := infrastruct.NewLogger().WithLevel(domain.Debug)
+	if len(os.Args) > 1 { // there are program args: worker.go ip port. Program name is included.
+		var port uint
+		_, err := fmt.Sscan(os.Args[2], &port)
+		if err != nil {
+			panic(err)
+		}
+		entry = &chord.RemoteNode{Ip: os.Args[1], Port: port}
+		log.Debug("entry set", domain.LogArgs{"entry": *entry})
+	}
+	//infrastruct.NewMainRoutine(entry)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	log.Info("worker up; stop it by pressing Ctrl+C", nil) // @todo show ip
+	<-c
+}
---
+++ worker.dockerfile
@@ -0,0 +1,15 @@
+FROM golang:1.18
+RUN ["mkdir", "e-tournaments"]
+WORKDIR /e-tournaments
+COPY utils utils
+COPY go.mod .
+COPY go.sum .
+COPY vendor vendor
+RUN [ "mkdir", "tournament" ]
+COPY tournament/domain tournament/domain
+COPY tournament/infrastruct tournament/infrastruct
+COPY tournament/interfaces tournament/interfaces
+COPY tournament/usecases tournament/usecases
+COPY tournament/worker.go tournament
+RUN go build tournament/worker.go
+ENTRYPOINT ["./worker"]
